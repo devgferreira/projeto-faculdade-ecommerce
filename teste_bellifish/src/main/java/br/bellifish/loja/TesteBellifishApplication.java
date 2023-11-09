abf821887c94f905b6388768299f48e05f833aa8
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-11-09 20:53:54 -0300

    feat: estrutura inicial do projeto

---
+++ teste_bellifish/src/main/java/br/bellifish/loja/TesteBellifishApplication.java
@@ -0,0 +1,13 @@
+package br.bellifish.loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteBellifishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteBellifishApplication.class, args);
+    }
+
+}
