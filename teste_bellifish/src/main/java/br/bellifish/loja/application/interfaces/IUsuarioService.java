70cba96e3793ac74a4103009342fdcd57b2d69a8
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-11-11 00:16:52 -0300

    feat: criando o contrato na interface da service: login

--- teste_bellifish/src/main/java/br/bellifish/loja/application/interfaces/IUsuarioService.java
+++ teste_bellifish/src/main/java/br/bellifish/loja/application/interfaces/IUsuarioService.java
@@ -1,10 +1,14 @@
 package br.bellifish.loja.application.interfaces;
 
+import br.bellifish.loja.application.dtos.LoginDTO;
 import br.bellifish.loja.application.dtos.UsuarioDTO;
 import br.bellifish.loja.application.dtos.UsuarioDTORequest;
+import br.bellifish.loja.domain.model.payload.LoginMessage;
 
 public interface IUsuarioService {
 
     UsuarioDTORequest criarUsuario(UsuarioDTORequest usuarioDTO);
     UsuarioDTO buscarUsuarioPorId(Long usuarioId);
+
+    LoginMessage login(LoginDTO loginDTO);
 }
