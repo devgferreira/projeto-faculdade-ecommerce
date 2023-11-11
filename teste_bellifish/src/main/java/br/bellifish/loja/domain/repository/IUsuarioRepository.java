64797d93eeac0c0178cb64bc6bad44116264f8f0
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-11-11 00:16:29 -0300

    feat: criando a query findByCpf e o findOneByCpfAndSenha no IUsuarioRepository

--- teste_bellifish/src/main/java/br/bellifish/loja/domain/repository/IUsuarioRepository.java
+++ teste_bellifish/src/main/java/br/bellifish/loja/domain/repository/IUsuarioRepository.java
@@ -4,6 +4,10 @@ import br.bellifish.loja.domain.model.user.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
+    Usuario findByCpf(String cpf);
+    Optional<Usuario> findOneByCpfAndSenha(String cpf, String senha);
 }
