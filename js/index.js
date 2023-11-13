f23c97bfd5ebdab4d3de37e0d5f0023d2357147d
Guilherme Bellini <140752834+DevBellini@users.noreply.github.com>
2023-11-13 16:51:49 -0300

    init: pagina principal

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinity: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+
+});
