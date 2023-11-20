30d85f9af91a38914383017546c06d58e0b16132
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-11-20 18:08:25 -0300

    feat: verificando se as senhas são compativeis no cadastro.js

--- front-end/js/cadastro.js
+++ front-end/js/cadastro.js
@@ -2,13 +2,20 @@ const formulario = document.querySelector("form");
 const botao = document.querySelector("button");
 const inome = document.querySelector(".nome");
 const isenha = document.querySelector(".senha");
+const isenhaConfirmada = document.querySelector(".senhaConfirmada");
 const itelefone = document.querySelector(".telefone");
 const iendereco = document.querySelector(".endereco");
 const icpf = document.querySelector(".cpf");
 
 
+if(isenha == isenhaConfirmada){
+
+}
+
 function cadastrar(){
-    fetch("http://localhost:8080/usuarios",
+    
+    if(isenha.value == isenhaConfirmada.value){
+        fetch("http://localhost:8080/usuarios",
         {
             headers: {
                 "Accept": "application/json",
@@ -25,11 +32,16 @@ function cadastrar(){
         })
         .then(function (res) { console.log(res)})
         .catch(function (res) {console.log(res)})
+    }else{
+        alert("A senha não são compativeis")
+    }
+    
 };
 
 function limpar(){
     inome.value = "";
     isenha.value = "";
+    isenhaConfirmada.value = "";
     itelefone.value = "";
     iendereco.value = "";
     icpf.value = "";
@@ -38,5 +50,7 @@ function limpar(){
 formulario.addEventListener('submit', function (event){
     event.preventDefault();
     cadastrar();
-    limpar();
+    if(isenha.value == isenhaConfirmada.value){
+        limpar();
+    }
 });
