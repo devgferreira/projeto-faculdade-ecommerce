b692b276831a8dba3eff4e5a224b989fe5ba8383
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-11-22 17:37:17 -0300

    feat: crianddo o response: AtualizarUsuarioResponse

---
+++ teste_bellifish/src/main/java/br/bellifish/loja/application/dtos/AtualizarUsuarioDTORequest.java
@@ -0,0 +1,14 @@
+package br.bellifish.loja.application.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AtualizarUsuarioDTORequest {
+    private String nome;
+    private Long telefone;
+    private String endereco;
+}
