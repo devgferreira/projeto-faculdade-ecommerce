86d04d785d7c2ea1e3f07740010d9ad348f88366
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-11-23 12:44:01 -0300

    feat: mudando o nome das pasta

--- teste_bellifish/src/main/java/br/bellifish/loja/infra/constants/ErrorConstants.java
+++
@@ -1,9 +0,0 @@
-package br.bellifish.loja.infra.constants;
-
-import lombok.AccessLevel;
-import lombok.Getter;
-
-@Getter(AccessLevel.PRIVATE)
-public class ErrorConstants {
-    public static final String CLIENTE_JA_EXISTE = " Cliente já existe";
-}
