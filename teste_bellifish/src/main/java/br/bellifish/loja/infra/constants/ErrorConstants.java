7cb0b9dbd8239e45675d509b519264cc8d89df49
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-11-23 12:29:10 -0300

    feat: criando as exceptions

---
+++ teste_bellifish/src/main/java/br/bellifish/loja/infra/constants/ErrorConstants.java
@@ -0,0 +1,9 @@
+package br.bellifish.loja.infra.constants;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+
+@Getter(AccessLevel.PRIVATE)
+public class ErrorConstants {
+    public static final String CLIENTE_JA_EXISTE = " Cliente já existe";
+}
